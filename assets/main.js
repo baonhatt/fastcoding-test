3787f7cf3d3d12ce4ca8298b91da31fc9c5f07a9
Bao Nhat <baonhat20@gmail.com>
2023-08-09 00:04:53 +0700

    fix2

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,9 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    const resBtn = document.querySelector(".res-btn");
+    const topNav = document.querySelector(".topnav");
+
+    resBtn.addEventListener("click", function () {
+        topNav.classList.toggle("active");
+    });
+});
