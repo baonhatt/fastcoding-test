7346d8430019ecd2042bdf7a70eef0d52e8bed1d
Bao Nhat <baonhat20@gmail.com>
2023-08-09 10:19:37 +0700

    responsive section

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,1 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-    const resBtn = document.querySelector(".res-btn");
-    const topNav = document.querySelector(".topnav");
-
-    resBtn.addEventListener("click", function () {
-        topNav.classList.toggle("active");
-    });
-});
